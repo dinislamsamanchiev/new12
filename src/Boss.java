825ad2e8aa153799e52b02ade1787bd74fc894dd
dinislamsamanchiev <dinislamsamanchiev27@gmail.com>
2022-07-28 17:31:53 +0900

    агай

---
+++ src/Boss.java
@@ -0,0 +1,30 @@
+public class Boss {
+    private int BossHealth;
+    private int BossDamage;
+    private String BossDefenseType;
+
+    public int getBossHealth() {
+        return BossHealth;
+    }
+
+    public int getBossDamage() {
+        return BossDamage;
+    }
+
+    public String getBossDefenseType() {
+        return BossDefenseType;
+    }
+
+    public void setBossHealth(int bossHealth) {
+        BossHealth = bossHealth;
+    }
+
+    public void setBossDamage(int bossDamage) {
+        BossDamage = bossDamage;
+    }
+
+    public void setBossDefenseType(String bossDefenseType) {
+        BossDefenseType = bossDefenseType;
+    }
+}
+
